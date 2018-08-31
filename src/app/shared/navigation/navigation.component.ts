65530b3536f69d1345b464594f3d1a344cf5a72e
vaniavc <vania@bagubagu.com>
2018-08-31 18:28:58 +0700

    responsive header

--- src/app/shared/navigation/navigation.component.ts
+++ src/app/shared/navigation/navigation.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, HostListener, Inject } from '@angular/core';
+import { DOCUMENT } from '@angular/platform-browser';
+import { WINDOW } from '../window.service';
 
 import { Flower } from 'src/app/flower';
 import { FLOWERS } from 'src/app/flowers';
@@ -9,9 +11,35 @@ import { FLOWERS } from 'src/app/flowers';
   styleUrls: ['./navigation.component.scss']
 })
 export class NavigationComponent implements OnInit {
+  public hamburger: boolean;
+  public scrolled: boolean;
+  public burger: boolean;
+
   @Input()
   flower: Flower;
-  constructor() {}
 
-  ngOnInit() {}
+  constructor(
+    @Inject(DOCUMENT) private document: Document,
+    @Inject(WINDOW) private window: Window
+  ) {}
+
+  ngOnInit() {
+    setTimeout(() => {
+      this.burger = false;
+    }, 500);
+  }
+
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    const number =
+      this.window.pageYOffset ||
+      this.document.documentElement.scrollTop ||
+      this.document.body.scrollTop ||
+      0;
+    if (number >= 60 && !this.hamburger) {
+      this.scrolled = true;
+    } else if (this.scrolled && number < 59) {
+      this.scrolled = false;
+    }
+  }
 }
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,26 +1,26 @@
 import { NgModule } from '@angular/core';
-//import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { RouterModule } from '@angular/router';
+import { WINDOW_PROVIDERS } from './window.service';
 
 import { NavigationComponent } from './navigation/navigation.component';
 import { FooterComponent } from './footer/footer.component';
 import { BoxComponent } from './box/box.component';
 
 @NgModule({
-    imports: [
-        CommonModule,
-        FlexLayoutModule,
-        RouterModule,
-    ],
-    exports: [
-        CommonModule,
-        FlexLayoutModule,
-        NavigationComponent,
-        FooterComponent,
-        BoxComponent
-    ],
-    declarations: [NavigationComponent, FooterComponent, BoxComponent]
+  imports: [CommonModule, FlexLayoutModule, RouterModule],
+  exports: [
+    CommonModule,
+    HttpClientModule,
+    FlexLayoutModule,
+    NavigationComponent,
+    FooterComponent,
+    BoxComponent
+  ],
+  providers: [WINDOW_PROVIDERS],
+
+  declarations: [NavigationComponent, FooterComponent, BoxComponent]
 })
-export class SharedModule { }
+export class SharedModule {}
